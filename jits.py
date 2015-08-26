a0803f40dd651fec84895b65e259dd973064ba00
Dave <davidjmcconnell@gmail.com>
2015-08-26 16:01:08 -0500

    Update jits.py
    Changed logic for determining sale line or void line in report file.  Removed redundent parentheses.

--- jits.py
+++ jits.py
@@ -14,9 +14,9 @@ with open('data/Report.csv', 'rb') as f:
 
     for row in reader:
 
-        if ( startData ):
+        if startData:
 
-            if row[0] not in countDict and len(row) > 2:
+            if row[0] not in countDict and len(row):
 
                 countDict[row[0]] = int(row[1])
                 saleString = row[2]
@@ -25,9 +25,9 @@ with open('data/Report.csv', 'rb') as f:
                 avgSale = sales / int(row[1])
                 # print 'SOLD:', int( row[1] ), row[0], 'at average', fpformat.fix( avgSale, 2 ), 'total', row[2]
 
-            elif len(row) > 4:
-                print len(row)
-                if ( row[4] != '' ):  # This item is a Sale
+            else:
+                
+                if len(row) == 6:  # This item is a Sale
 
                     currentCount = countDict[row[0]]
                     newCount = currentCount + int(row[1])
@@ -43,20 +43,20 @@ with open('data/Report.csv', 'rb') as f:
                     # print 'UPDATE COUNT:', row[0], 'to', newCount, 'at', fpformat.fix( avgSale, 2 )
                     #print 'UPDATE SALES:', row[0], 'to', currentSales + newSales, 'total'
 
-                else:  # This item is a Void
+                elif len(row) == 4:  # This item is a Void
 
                     currentCount = countDict[row[0]]
                     newCount = currentCount - int(row[1])
                     countDict[row[0]] = newCount
                     # print 'VOID: ',int( row[1] ), row[0], 'to', newCount
 
-        if (len(row) > 0):
+        if len(row):
             if row[0] == 'Item Name':
                 startData = True
 
     for item in countDict:
 
-        if ( countDict[item] > 0 ):
+        if countDict[item] > 0:
             avgSale = fpformat.fix(salesDict[item] / countDict[item], 2)
             total = fpformat.fix(salesDict[item], 2)
 
