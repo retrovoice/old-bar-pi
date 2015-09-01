2e5f86a920099e6b372a906fc534ba6c928a61ec
Dave McConnell <david.j.mcconnell.civ@mail.mil>
2015-09-01 12:38:03 -0500

    Update test data file name.  Converted syntax to use Python 3.

--- jits.py
+++ jits.py
@@ -1,22 +1,21 @@
 __author__ = 'mcconnelldj'
 
 import csv
-import fpformat
 import locale
 
 countDict = {}
 salesDict = {}
 locale.setlocale(locale.LC_ALL, '')
 
-with open('data/TouchBistroReport.csv', 'rb') as f:
+with open('data/Report.csv') as f:
     startData = False
     reader = csv.reader(f)
 
     for row in reader:
 
-        if ( startData ):
+        if startData:
 
-            if row[0] not in countDict and len(row) > 2:
+            if row[0] not in countDict and len(row):
 
                 countDict[row[0]] = int(row[1])
                 saleString = row[2]
@@ -25,9 +24,9 @@ with open('data/TouchBistroReport.csv', 'rb') as f:
                 avgSale = sales / int(row[1])
                 # print 'SOLD:', int( row[1] ), row[0], 'at average', fpformat.fix( avgSale, 2 ), 'total', row[2]
 
-            elif len(row) > 4:
-                print len(row)
-                if ( row[4] != '' ):  # This item is a Sale
+            else:
+
+                if len(row) == 6:  # This item is a Sale
 
                     currentCount = countDict[row[0]]
                     newCount = currentCount + int(row[1])
@@ -43,21 +42,21 @@ with open('data/TouchBistroReport.csv', 'rb') as f:
                     # print 'UPDATE COUNT:', row[0], 'to', newCount, 'at', fpformat.fix( avgSale, 2 )
                     #print 'UPDATE SALES:', row[0], 'to', currentSales + newSales, 'total'
 
-                else:  # This item is a Void
+                elif len(row) == 4:  # This item is a Void
 
                     currentCount = countDict[row[0]]
                     newCount = currentCount - int(row[1])
                     countDict[row[0]] = newCount
                     # print 'VOID: ',int( row[1] ), row[0], 'to', newCount
 
-        if (len(row) > 0):
+        if len(row):
             if row[0] == 'Item Name':
                 startData = True
 
     for item in countDict:
 
-        if ( countDict[item] > 0 ):
-            avgSale = fpformat.fix(salesDict[item] / countDict[item], 2)
-            total = fpformat.fix(salesDict[item], 2)
+        if countDict[item] > 0:
+            avgSale = '${:.2f}'.format(salesDict[item] / countDict[item])
+            total = '${:.2f}'.format(salesDict[item])
 
-            print 'SOLD', countDict[item], item, 'at', avgSale, 'each, total =', total
+            print('SOLD', countDict[item], item, 'at', avgSale, 'each, total =', total)
