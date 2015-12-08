2f663910debaa9021ec616cdfe86137709ff05b6
Dave McConnell <david.j.mcconnell.civ@mail.mil>
2015-12-08 11:59:54 -0600

    Buttons are now in a group so that only one button can be pressed at a time. Added label for top of window, but no formatting for it yet.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,15 +17,35 @@ MainWindow::MainWindow(QString configFile):
 void MainWindow::createLayout()
 {
 	button01 = new QPushButton("Catalogue");
+	button01->setCheckable(true);
+	button01->setChecked(true);
 	button02 = new QPushButton("Iventory");
+	button02->setCheckable(true);
+	button02->setChecked(false);
 	button03 = new QPushButton("Measure");
+	button03->setCheckable(true);
+	button03->setChecked(false);
 	button04 = new QPushButton("Setup");
+	button04->setCheckable(true);
+	button04->setChecked(false);
 
 	QGridLayout *gLayout = new QGridLayout;
-	gLayout->addWidget(button01,0,0);
-	gLayout->addWidget(button02,1,0);
-	gLayout->addWidget(button03,2,0);
-	gLayout->addWidget(button04,3,0);
+	QButtonGroup *row01Group = new QButtonGroup;
+
+	QLabel *banner = new QLabel("BarPi");
+	gLayout->addWidget(banner,0,0,1,3);
+
+
+	row01Group->addButton(button01,1);
+	row01Group->addButton(button02,2);
+	row01Group->addButton(button03,3);
+	row01Group->addButton(button04,4);
+
+	gLayout->addWidget(button01,1,0);
+	gLayout->addWidget(button02,2,0);
+	gLayout->addWidget(button03,3,0);
+	gLayout->addWidget(button04,4,0);
+
 	setLayout(gLayout);
 
 }
