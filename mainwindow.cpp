703ba34be5333d5a0fbdd0e7f2fe324145d5efba
Dave McConnell <david.j.mcconnell.civ@mail.mil>
2015-12-08 16:30:01 -0600

    Incremental progress.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,35 +16,158 @@ MainWindow::MainWindow(QString configFile):
 
 void MainWindow::createLayout()
 {
-	button01 = new QPushButton("Catalogue");
-	button01->setCheckable(true);
-	button01->setChecked(true);
-	button02 = new QPushButton("Iventory");
-	button02->setCheckable(true);
-	button02->setChecked(false);
-	button03 = new QPushButton("Measure");
-	button03->setCheckable(true);
-	button03->setChecked(false);
-	button04 = new QPushButton("Setup");
-	button04->setCheckable(true);
-	button04->setChecked(false);
-
+	// The layout for this window will be a grid.
 	QGridLayout *gLayout = new QGridLayout;
-	QButtonGroup *row01Group = new QButtonGroup;
 
-	QLabel *banner = new QLabel("BarPi");
+	QLabel *banner = new QLabel();
+	banner->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+	//banner->setTextFormat(Qt::RichText);
+	banner->setText("Barpi");
+
 	gLayout->addWidget(banner,0,0,1,3);
 
+	// These 4 buttons make up the main menu of the app.
+	// Set the buttons Checkable, and make one of
+	// them Checked for mutual exclusive operation
+	// when added to the button group.
+	mp_button10 = new QPushButton("Catalogue");
+	mp_button10->setCheckable(true);
+	mp_button10->setChecked(true);
+	mp_button20 = new QPushButton("Stock");
+	mp_button20->setCheckable(true);
+	mp_button20->setChecked(false);
+	mp_button30 = new QPushButton("Inventory");
+	mp_button30->setCheckable(true);
+	mp_button30->setChecked(false);
+	mp_button40 = new QPushButton("Setup");
+	mp_button40->setCheckable(true);
+	mp_button40->setChecked(false);
 
-	row01Group->addButton(button01,1);
-	row01Group->addButton(button02,2);
-	row01Group->addButton(button03,3);
-	row01Group->addButton(button04,4);
+	// Add items to a button group which will enforce
+	// mutual exclusion, allowing only one of them
+	// to be active at a time.
+	QButtonGroup *col01Group = new QButtonGroup;
+	col01Group->addButton(mp_button10,1);
+	col01Group->addButton(mp_button20,2);
+	col01Group->addButton(mp_button30,3);
+	col01Group->addButton(mp_button40,4);
 
-	gLayout->addWidget(button01,1,0);
-	gLayout->addWidget(button02,2,0);
-	gLayout->addWidget(button03,3,0);
-	gLayout->addWidget(button04,4,0);
+	mp_button11 = new QPushButton("Option 11");
+	mp_button11->setCheckable(true);
+	mp_button11->setChecked(true);
+	mp_button12 = new QPushButton("Option 12");
+	mp_button12->setCheckable(true);
+	mp_button12->setChecked(false);
+	mp_button13 = new QPushButton("Option 13");
+	mp_button13->setCheckable(true);
+	mp_button13->setChecked(false);
+	mp_button14 = new QPushButton("Option 14");
+	mp_button14->setCheckable(true);
+	mp_button14->setChecked(false);
+
+	mp_button21 = new QPushButton("Option 21");
+	mp_button21->setCheckable(true);
+	mp_button21->setChecked(true);
+	mp_button22 = new QPushButton("Option 22");
+	mp_button22->setCheckable(true);
+	mp_button22->setChecked(false);
+	mp_button23 = new QPushButton("Option 23");
+	mp_button23->setCheckable(true);
+	mp_button23->setChecked(false);
+	mp_button24 = new QPushButton("Option 24");
+	mp_button24->setCheckable(true);
+	mp_button24->setChecked(false);
+
+	mp_button31 = new QPushButton("Option 31");
+	mp_button31->setCheckable(true);
+	mp_button31->setChecked(true);
+	mp_button32 = new QPushButton("Option 32");
+	mp_button32->setCheckable(true);
+	mp_button32->setChecked(false);
+	mp_button33 = new QPushButton("Option 33");
+	mp_button33->setCheckable(true);
+	mp_button33->setChecked(false);
+	mp_button34 = new QPushButton("Option 34");
+	mp_button34->setCheckable(true);
+	mp_button34->setChecked(false);
+
+	mp_button41 = new QPushButton("Option 41");
+	mp_button41->setCheckable(true);
+	mp_button41->setChecked(true);
+	mp_button42 = new QPushButton("Option 42");
+	mp_button42->setCheckable(true);
+	mp_button42->setChecked(false);
+	mp_button43 = new QPushButton("Option 43");
+	mp_button43->setCheckable(true);
+	mp_button43->setChecked(false);
+	mp_button44 = new QPushButton("Option 44");
+	mp_button44->setCheckable(true);
+	mp_button44->setChecked(false);
+
+	QButtonGroup *page1Group = new QButtonGroup;
+	page1Group->addButton(mp_button11,11);
+	page1Group->addButton(mp_button12,12);
+	page1Group->addButton(mp_button13,13);
+	page1Group->addButton(mp_button14,14);
+
+	QButtonGroup *page2Group = new QButtonGroup;
+	page2Group->addButton(mp_button21,21);
+	page2Group->addButton(mp_button22,22);
+	page2Group->addButton(mp_button23,23);
+	page2Group->addButton(mp_button24,24);
+
+	QButtonGroup *page3Group = new QButtonGroup;
+	page3Group->addButton(mp_button31,31);
+	page3Group->addButton(mp_button32,32);
+	page3Group->addButton(mp_button33,33);
+	page3Group->addButton(mp_button34,34);
+
+	QButtonGroup *page4Group = new QButtonGroup;
+	page4Group->addButton(mp_button41,41);
+	page4Group->addButton(mp_button42,42);
+	page4Group->addButton(mp_button43,43);
+	page4Group->addButton(mp_button44,44);
+
+	QVBoxLayout *page1Layout = new QVBoxLayout;
+	page1Layout->addWidget(mp_button11);
+	page1Layout->addWidget(mp_button12);
+	page1Layout->addWidget(mp_button13);
+	page1Layout->addWidget(mp_button14);
+
+	QVBoxLayout *page2Layout = new QVBoxLayout;
+	page2Layout->addWidget(mp_button21);
+	page2Layout->addWidget(mp_button22);
+	page2Layout->addWidget(mp_button23);
+	page2Layout->addWidget(mp_button24);
+
+	QVBoxLayout *page3Layout = new QVBoxLayout;
+	page3Layout->addWidget(mp_button31);
+	page3Layout->addWidget(mp_button32);
+	page3Layout->addWidget(mp_button33);
+	page3Layout->addWidget(mp_button34);
+
+	QVBoxLayout *page4Layout = new QVBoxLayout;
+	page4Layout->addWidget(mp_button41);
+	page4Layout->addWidget(mp_button42);
+	page4Layout->addWidget(mp_button43);
+	page4Layout->addWidget(mp_button44);
+
+	QWidget *page1 = new QWidget;
+	page1->setLayout(page1Layout);
+	QWidget *page2 = new QWidget;
+	page1->setLayout(page2Layout);
+	QWidget *page3 = new QWidget;
+	page1->setLayout(page3Layout);
+	QWidget *page4 = new QWidget;
+	page1->setLayout(page4Layout);
+	
+	QStackedLayout *pagesLayout = new QStackedLayout;
+	pagesLayout->addWidget(page1);
+	pagesLayout->addWidget(page2);
+	pagesLayout->addWidget(page3);
+	pagesLayout->addWidget(page4);
+
+	gLayout->addLayout(pagesLayout,1,1,4,1);
 
 	setLayout(gLayout);
 
--- mainwindow.h
+++ mainwindow.h
@@ -22,10 +22,30 @@ public:
 
 private:
 	void createLayout();
-	QPushButton *button01;
-	QPushButton *button02;
-	QPushButton *button03;
-	QPushButton *button04;
+	QPushButton *mp_button10;
+	QPushButton *mp_button20;
+	QPushButton *mp_button30;
+	QPushButton *mp_button40;
+
+	QPushButton *mp_button11;
+	QPushButton *mp_button12;
+	QPushButton *mp_button13;
+	QPushButton *mp_button14;
+
+	QPushButton *mp_button21;
+	QPushButton *mp_button22;
+	QPushButton *mp_button23;
+	QPushButton *mp_button24;
+
+	QPushButton *mp_button31;
+	QPushButton *mp_button32;
+	QPushButton *mp_button33;
+	QPushButton *mp_button34;
+
+	QPushButton *mp_button41;
+	QPushButton *mp_button42;
+	QPushButton *mp_button43;
+	QPushButton *mp_button44;
 
 	QString m_currentConfigFile;
 };
