14806a32e16aaa14c4d27c46fe96f70e407ea293
Dave McConnell <davidjmcconnell@gmail.com>
2016-10-08 09:14:52 -0500

    Added some font stuff to the QApplication in an attempt to get characters to display when compiling/running on Kubuntu 16.04 plasma. The application windows function but there is no text visible. This change did not solve the problem.  Switching to RPi to see if I can continue development there.
     On branch devworking
     Your branch is up-to-date with 'origin/devworking'.
    
     Changes to be committed:
    	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <QApplication>
+//#include <QGuiApplication>
 #include <QString>
 #include <QStringList>
+#include <QFont>
 #include "mainwindow.h"
 #include "commandlineparser.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+
+    QFont sansFont("Sans [DejaVu]",12);
+
+    QApplication::setFont(sansFont);
 
 	// Store the command line arguments in a list
 	QStringList *args = new QStringList(QApplication::arguments());
