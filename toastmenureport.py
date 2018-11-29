c53d5fd376bbae92ecac0fd6f10d514895765c34
Dave McConnell <davidjmcconnell@gmail.com>
2018-11-28 18:35:36 -0600

    Created .gitignore file and updates to toast menu report.

--- toastmenureport.py
+++ toastmenureport.py
@@ -53,8 +53,8 @@ def processMenu(csvFile):
                 # Convert Master ID text value to integer for use as dictionary key
                 # Use only the characters from index 10 to the end, as the numbers
                 # seem to all have 40000000000 as a prefix
-                shortID = line[0].lstrip('40')
-                #shortID = longID
+                longID = line[0].lstrip('4')
+                shortID = longID.lstrip('0')
                 int_key = locale.atoi(shortID)
 
                 # This is a new key, so simply add it to the dictionary
@@ -96,179 +96,90 @@ def consolidateWine():
     # glass sales into a single quantity.  It uses a dictionary
     # with the name of the wine as the key.
 
+    # Factor for glasses per bottle
+    toBottle = 177.441/750.
     #print(masterDict["400000000005280263"][8] + ", " + masterDict["400000000005280373"][8])
 
-    whiteWineDict["Promesso"] = masterDict[5280263][7] * .177441 \
-                                + masterDict[5280373][7] * .750
-    redWineDict["Tangley Oaks"] = masterDict[5280170][7] * .177441 \
-                                  + masterDict[5280233][7] * .750
-    redWineDict["Val Beylie"] = masterDict[5280172][7] * .177441 \
-                                + masterDict[5280235][7] * .750
-    redWineDict["Campolieti"] = masterDict[5280174][7] * .177441 \
-                                + masterDict[5280237][7] * .750
-    redWineDict["Vina Amalia Reserva Malbec"] = masterDict[5280176][7] * .177441 \
-                                                + masterDict[5280239][7] * .750
-    redWineDict["Querceto Chianti"] = masterDict[5280178][7] * .177441 \
-                                      + masterDict[5280241][7] * .750
-    redWineDict["Villa Barbi Rosso"] = masterDict[5280180][7] * .177441 \
-                                       + masterDict[5280247][7] * .750
-    redWineDict["Belleruche CDR"] = masterDict[5280184][7] * .177441 \
-                        + masterDict[5280301][7] * .750
-    redWineDict["Lyric Pinot Noir"] = masterDict[5280186][7] * .177441 \
-                        + masterDict[5280305][7] * .750
-    redWineDict["Tamari Cab"] = masterDict[5280188][7] * .177441 \
-                        + masterDict[5280309][7] * .750
-    redWineDict["Joel Gott Cab"] = masterDict[5280190][7] * .177441 \
-                        + masterDict[5280311][7] * .750
-    redWineDict["Oberon Cab"] = masterDict[5280192][7] * .177441 \
-                        + masterDict[5280313][7] * .750
+    whiteWineDict["Promesso"] = masterDict[5280263][7] * toBottle \
+                                + masterDict[5280373][7]
+    whiteWineDict["Montevina White Zin"] = masterDict[20379824][7] * toBottle \
+                                           + masterDict[20379189][7]
+    whiteWineDict["Cuvee Van Gogh"] = masterDict[5280259][7] * toBottle \
+                                      + masterDict[5280369][7]
+    whiteWineDict["Selbach Incline"] = masterDict[5280261][7] * toBottle \
+                                       + masterDict[5280371][7]
+    whiteWineDict["Torrontes La Yunta"] = masterDict[5280265][7] * toBottle \
+                                          + masterDict[5280377][7]
+    whiteWineDict["Soave DOP"] = masterDict[5280267][7] * toBottle \
+                                 + masterDict[5280379][7]
+    whiteWineDict["Whitehaven"] = masterDict[5280269][7] * toBottle \
+                                  + masterDict[5280383][7]
+    whiteWineDict["Heron Chardonnay"] = masterDict[5280271][7] * toBottle \
+                                        + masterDict[5280387][7]
+    whiteWineDict["Lyric Chardonnay"] = masterDict[20379822][7] * toBottle \
+                                        + masterDict[5280389][7]
+    whiteWineDict["Luminus Chardonnay"] = masterDict[5280273][7] * toBottle \
+                                          + masterDict[5280391][7]
+    whiteWineDict["Colduldrum"] = masterDict[113382578][7] * toBottle \
+                                  + masterDict[113386926][7]
+
+    redWineDict["Tangley Oaks"] = masterDict[5280170][7] * toBottle \
+                                  + masterDict[5280233][7]
+    redWineDict["Val Beylie"] = masterDict[5280172][7] * toBottle \
+                                + masterDict[5280235][7]
+    redWineDict["Campolieti"] = masterDict[5280174][7] * toBottle \
+                                + masterDict[5280237][7]
+    redWineDict["Vina Amalia Reserva Malbec"] = masterDict[5280176][7] * toBottle \
+                                                + masterDict[5280239][7]
+    redWineDict["Querceto Chianti"] = masterDict[5280178][7] * toBottle \
+                                      + masterDict[5280241][7]
+    redWineDict["Villa Barbi Rosso"] = masterDict[5280180][7] * toBottle \
+                                       + masterDict[5280247][7]
+    redWineDict["Belleruche CDR"] = masterDict[5280184][7] * toBottle \
+                                    + masterDict[5280301][7]
+    redWineDict["Lyric Pinot Noir"] = masterDict[5280186][7] * toBottle \
+                                      + masterDict[5280305][7]
+    redWineDict["Tamari Cab"] = masterDict[5280188][7] * toBottle \
+                                + masterDict[5280309][7]
+    redWineDict["Joel Gott Cab"] = masterDict[5280190][7] * toBottle \
+                                   + masterDict[5280311][7]
+    redWineDict["Oberon Cab"] = masterDict[5280192][7] * toBottle \
+                                + masterDict[5280313][7]
+    redWineDict["Federalist Zinfandel"] = masterDict[20378197][7] * toBottle \
+                                          + masterDict[20376740][7]
+    redWineDict["Sangue Di Guida"] = masterDict[20378199][7] * toBottle \
+                                     + masterDict[20376746][7]
+    redWineDict["Beaujolais"] = masterDict[20378301][7] * toBottle \
+                                + masterDict[20376736][7]
+    redWineDict["Durigutti Cabernet Franc"] = masterDict[21944774][7] * toBottle \
+                                              + masterDict[21941608][7]
+    redWineDict["Dos Fincas Cab Sauv Malbec"] = masterDict[21944776][7] * toBottle \
+                                                + masterDict[21941610][7]
+    redWineDict["Velvet Devil"] = masterDict[37305962][7] * toBottle \
+                                  + masterDict[33865502][7]
     print (whiteWineDict)
     print (redWineDict)
 
-    # # Create file for wine sales and output data.
-    # nameWine = startEndDate + '_wineSales.csv'
-    # wineSales = open(nameWine,'w')
-    # wineSales.write('CONSOLIDATED WINE SALES,,' + startEndDate + '\n\n')
-    # wineSales.write('Wine,Bottles Sold,Avg. Price per Bottle\n')
-    # for wine in consolidatewine:
-    #     bottles = 0
-    #     if consolidatewine[wine][0] == 0:
-    #         avgp = '0'
-    #         pbottles = '0'
-    #         outline = wine + ',0,0\n'
-    #     else:
-    #         bottles = consolidatewine[wine][0] / 750.
-    #         pbottles = '{:.2f}'.format(bottles)
-    #         avgp = '{:.2f}'.format(consolidatewine[wine][1] / bottles)
-    #         outline = wine + ',' + pbottles + ',' + avgp + '\n'
-    #     wineSales.write(outline)
-    #
-    # # Consolidate liquor variants of the same product into
-    # # one total quantity, e.g. regular (no suffix), 2oz, Up pour
-    # consolidateliquor = {}
-    # # Need a separate dictionary for named cocktails
-    # for menuitem in alcoholDict:
-    #     # make sure there's no trailing white space
-    #     item = menuitem.rstrip()
-    #     # determine the volume based on regular shot, 2oz or Up
-    #     volume = 0
-    #     sales = 0
-    #     root = ''
-    #     if (item[-4:] == ' 2oz'):
-    #         root = item[:-4]
-    #         volume = alcoholDict[menuitem][0] * 59.1471
-    #         sales = alcoholDict[menuitem][1]
-    #     elif (item[-3:] == ' Up'):
-    #         root = item[:-3]
-    #         volume = alcoholDict[menuitem][0] * 88.7206
-    #         sales = alcoholDict[menuitem][1]
-    #     else:
-    #         root = item
-    #         if root == 'Martini':
-    #             root = 'Vodka'
-    #             volume = alcoholDict[menuitem][0] * 88.7206
-    #             sales = alcoholDict[menuitem][1]
-    #         elif root == 'Bloody Mary':
-    #             root = 'Vodka'
-    #             volume = alcoholDict[menuitem][0] * 59.1471
-    #             sales = alcoholDict[menuitem][1]
-    #         elif root[-9:] == 'Margarita':
-    #             root = 'Tequila'
-    #             volume = alcoholDict[menuitem][0] * 36.96691
-    #             sales = alcoholDict[menuitem][1]
-    #         elif root == 'Hendricks Mule':
-    #             root = 'Hendricks Gin'
-    #             volume = alcoholDict[menuitem][0] * 59.1471
-    #             sales = alcoholDict[menuitem][1]
-    #         elif root == 'Apple Bourbon Cool Toddy':
-    #             root = 'Coopers Craft'
-    #             volume = alcoholDict[menuitem][0] * 59.1471
-    #             sales = alcoholDict[menuitem][1]
-    #         elif root == 'Irish Coffee':
-    #             root = 'Bushmills'
-    #             volume = alcoholDict[menuitem][0] * 36.96691
-    #             sales = alcoholDict[menuitem][1]
-    #         else:
-    #             volume = alcoholDict[menuitem][0] * 36.96691
-    #             sales = alcoholDict[menuitem][1]
-    #
-    #     if (root not in consolidateliquor and root != ''):
-    #         consolidateliquor[root] = (volume, sales)
-    #     elif (root != ''):
-    #         currentVolume = consolidateliquor[root][0]
-    #         newVolume = currentVolume + volume
-    #         currentSales = consolidateliquor[root][1]
-    #         newSales = currentSales + sales
-    #         consolidateliquor[root] = (newVolume, newSales)
-    #
-    # # Create file for liquor, cocktail and beer sales, and output data.
-    # lcbFilename = startEndDate + '_LCB.csv'
-    # lcbSales = open(lcbFilename,'w')
-    # lcbSales.write('LIQUOR COCKTAIL AND BEER SALES,' + startEndDate + '\n\n')
-    # lcbSales.write('Liquor,Liters,Shots,Avg price/shot\n')
-    # for liquor in consolidateliquor:
-    #     liters = 0
-    #     if consolidateliquor[liquor][0] == 0:
-    #         avgp = '0'
-    #         pml = '0'
-    #         pshots = '0'
-    #         outline = liquor + ',0,0,0\n'
-    #     else:
-    #         liters = consolidateliquor[liquor][0] / 1000
-    #         shots = liters/.03696691
-    #         pml = '{:.2f}'.format(liters)
-    #         pshots = '{:.2f}'.format(shots)
-    #         avgp = '{:.2f}'.format(consolidateliquor[liquor][1] / shots)
-    #         outline = liquor + ',' + pml + ',' + pshots + ','+ avgp + '\n'
-    #     lcbSales.write(outline)
-    #
-    # # Write out cocktail sales to the file
-    # lcbSales.write('\nCocktail,Sold,Avg price\n')
-    # for cocktail in cocktailDict:
-    #     if cocktailDict[cocktail][0] == 0:
-    #         avgp = '0'
-    #         pcount = '0'
-    #         outline = cocktail + ',0,0\n'
-    #     else:
-    #         count = cocktailDict[cocktail][0]
-    #         pcount = '{:.2f}'.format(count)
-    #         avgp = '{:.2f}'.format(cocktailDict[cocktail][1] / count)
-    #         outline = cocktail + ',' + pcount + ',' + avgp + '\n'
-    #     lcbSales.write(outline)
-    #
-    # # Write out beer sales to the file
-    # lcbSales.write('\nBeer,Sold,Avg price\n')
-    # for bottle in beerDict:
-    #     if beerDict[bottle][0] == 0:
-    #         avgp = '0'
-    #         outline = bottle + ',0,0\n'
-    #     else:
-    #         count = beerDict[bottle][0]
-    #         pcount = '{:.2f}'.format(count)
-    #         avgp = '{:.2f}'.format(beerDict[bottle][1] / count)
-    #         outline = bottle + ',' + pcount + ','  + avgp + '\n'
-    #     lcbSales.write(outline)
-    #
-    #
-    # # Create file for food sales, and output data.
-    # foodFilename = startEndDate + '_Food.csv'
-    # foodSales = open(foodFilename,'w')
-    # foodSales.write('FOOD SALES,' + startEndDate + '\n\n')
-    # foodSales.write('Category,Menu Item,Sold,Avg price\n')
-    # for plate in foodDict:
-    #     parts = plate.split('::')
-    #     cat = parts[0]
-    #     name = parts[1]
-    #     if foodDict[plate][0] == 0:
-    #         avgp = 0
-    #         outline = plate + ',0,0\n'
-    #     else:
-    #         count = foodDict[plate][0]
-    #         pcount = '{:.2f}'.format(count)
-    #         avgp = '{:.2f}'.format(foodDict[plate][1] / count)
-    #         outline = cat + ',' + name + ',' + pcount + ',' + avgp + '\n'
-    #     foodSales.write(outline)
-    #
+    # Create file for wine sales and output data.
+    nameWine = 'WineSales.csv'
+    wineSales = open(nameWine,'w')
+    wineSales.write('CONSOLIDATED WINE SALES\n\n')
+    wineSales.write('Wine,Bottles Sold,Avg. Price per Bottle\n')
+    for wine in whiteWineDict:
+        bottles = 0
+        #avgp = '0'
+        pbottles = '0'
+        outline = ''
+        if whiteWineDict[wine] == 0.:
+            outline = wine + ',0\n'
+        else:
+            bottles = whiteWineDict[wine]
+            pbottles = '{:.2f}'.format(bottles)
+            #avgp = '{:.2f}'.format(whiteWineDict[wine][1] / bottles)
+            #outline = wine + ',' + pbottles + ',' + avgp + '\n'
+            outline = wine + ',' + pbottles + '\n'
+        wineSales.write(outline)
+    wineSales.close()
     return 0
 
 ##### _Main_
